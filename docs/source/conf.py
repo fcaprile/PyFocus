244ba8f0f6b9f62e85d2b72370394c5130eefd6b
fcaprile <fcaprile@gmail.com>
2021-07-01 15:46:22 -0300

    d

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,8 +13,11 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
-
-
+sys.path.insert(0, os.path.abspath('./.'))
+sys.path.insert(0, os.path.abspath('/.'))
+sys.path.insert(0, os.path.abspath('../.'))
+sys.path.insert(0, os.path.abspath('.../.'))
+sys.path.insert(0, os.path.abspath('.../...'))
 # -- Project information -----------------------------------------------------
 
 project = 'PyFocus'
