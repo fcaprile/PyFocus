0f0f236db1b30ada01a645c68f968ee7e7697211
fcaprile <fcaprile@gmail.com>
2021-07-21 23:42:31 -0300

    Revert "Update conf.py"
    This reverts commit c77e9eef3bc9779c2cc0ac7ae2f363e7dcd1d3ed.

--- PyFocus/docs/source/conf.py
+++ PyFocus/docs/source/conf.py
@@ -18,12 +18,6 @@ sys.path.insert(0, os.path.abspath('/.'))
 sys.path.insert(0, os.path.abspath('../.'))
 sys.path.insert(0, os.path.abspath('.../.'))
 sys.path.insert(0, os.path.abspath('.../...'))
-sys.path.insert(0, os.path.abspath('.../..'))
-sys.path.insert(0, os.path.abspath('..../'))
-sys.path.insert(0, os.path.abspath('..../.'))
-sys.path.insert(0, os.path.abspath('..../..'))
-sys.path.insert(0, os.path.abspath('..../...'))
-sys.path.insert(0, os.path.abspath('..../....'))
 # -- Project information -----------------------------------------------------
 
 project = 'PyFocus'
