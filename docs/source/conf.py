574cbf47516180169edae42749fdcc343ef074b9
fcaprile <fcaprile@gmail.com>
2021-07-22 00:41:01 -0300

    redone html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,15 +12,15 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('./'))
 sys.path.insert(0, os.path.abspath('./.'))
-sys.path.insert(0, os.path.abspath('/.'))
-sys.path.insert(0, os.path.abspath('../.'))
-sys.path.insert(0, os.path.abspath('.../.'))
-sys.path.insert(0, os.path.abspath('.../...'))
-sys.path.insert(0, os.path.abspath('.../'))
 sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../auxiliary'))
+sys.path.insert(0, os.path.abspath('../.'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('.../'))
+sys.path.insert(0, os.path.abspath('.../.'))
+sys.path.insert(0, os.path.abspath('.../..'))
+sys.path.insert(0, os.path.abspath('.../...'))
 # -- Project information -----------------------------------------------------
 
 project = 'PyFocus'
