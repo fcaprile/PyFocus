c77e9eef3bc9779c2cc0ac7ae2f363e7dcd1d3ed
fcaprile <fcaprile@gmail.com>
2021-07-21 22:51:54 -0300

    Update conf.py

--- PyFocus/docs/source/conf.py
+++ PyFocus/docs/source/conf.py
@@ -18,6 +18,12 @@ sys.path.insert(0, os.path.abspath('/.'))
 sys.path.insert(0, os.path.abspath('../.'))
 sys.path.insert(0, os.path.abspath('.../.'))
 sys.path.insert(0, os.path.abspath('.../...'))
+sys.path.insert(0, os.path.abspath('.../..'))
+sys.path.insert(0, os.path.abspath('..../'))
+sys.path.insert(0, os.path.abspath('..../.'))
+sys.path.insert(0, os.path.abspath('..../..'))
+sys.path.insert(0, os.path.abspath('..../...'))
+sys.path.insert(0, os.path.abspath('..../....'))
 # -- Project information -----------------------------------------------------
 
 project = 'PyFocus'
