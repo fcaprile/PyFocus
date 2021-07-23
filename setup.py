f55eb6e21205ca347770adf69747b463ecf5a936
fcaprile <fcaprile@gmail.com>
2021-07-22 23:38:09 -0300

    changing requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'PyCustomFocus',         # How you named your package folder (MyLib)
   packages = ['PyFocus'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.2.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Full vectorial calculation of focused electromagnetic fields moduled by a custom phase mask',   # Give a short description about your library
   author = 'Caprile Fernando',                   # Type in your name
@@ -12,7 +12,6 @@ setup(
   keywords = ['User interface', 'Custom phase mask'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
     'config',
-    'tqdm',
     'numpy',
     'matplotlib',
     'scipy',
