fde519bf4e8ef6556bc5b2ceedc28ff51947f730
fcaprile <fcaprile@gmail.com>
2021-07-22 23:25:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'PyCustomFocus',         # How you named your package folder (MyLib)
   packages = ['PyFocus'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Full vectorial calculation of focused electromagnetic fields moduled by a custom phase mask',   # Give a short description about your library
   author = 'Caprile Fernando',                   # Type in your name
