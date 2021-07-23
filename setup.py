0974de149e11c9223a62b346f58804d693868ffb
fcaprile <fcaprile@gmail.com>
2021-07-22 23:53:33 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     'scipy',
     'qdarkstyle',
     'configparser',
-    'time'
 ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
