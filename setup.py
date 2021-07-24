526077e224ae2652a9b9962d57d6713ba6303179
fcaprile <fcaprile@gmail.com>
2021-07-23 23:08:38 -0300

    improved examples

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'PyCustomFocus',         
   packages = ['PyFocus'],   
-  version = '0.3.5',      # Start with a small number and increase it with every change you make
+  version = '0.3.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Full vectorial calculation of focused electromagnetic fields moduled by a custom phase mask',   # Give a short description about your library
   author = 'Caprile Fernando',                   # Type in your name
   author_email = 'fcaprile@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/fcaprile/PyFocus',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/fcaprile/PyFocus/archive/refs/tags/0.3.5.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/fcaprile/PyFocus/archive/refs/tags/0.3.6.tar.gz',    # I explain this later on
   keywords = ['User interface', 'Custom phase mask'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
     'config',
