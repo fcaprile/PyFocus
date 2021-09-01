48fc5e0c12e035ef1e59ac8c4b090a857e0be6fb
fcaprile <fcaprile@gmail.com>
2021-08-31 21:44:00 -0300

    prepare for updating new version to Pypi

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from distutils.core import setup
 setup(
   name = 'PyCustomFocus',         
   packages = ['PyFocus'],   
-  version = '0.4.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Full vectorial calculation of focused electromagnetic fields moduled by a custom phase mask',   # Give a short description about your library
-  author = 'Caprile Fernando',                   # Type in your name
-  author_email = 'fcaprile@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/fcaprile/PyFocus',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/fcaprile/PyFocus/archive/refs/tags/0.4.1.tar.gz',    # I explain this later on
+  version = '1.0',      
+  license='MIT',        
+  description = 'Full vectorial calculation of focused electromagnetic fields moduled by a custom phase mask',   
+  author = 'Caprile Fernando',                  
+  author_email = 'fcaprile@gmail.com',      
+  url = 'https://github.com/fcaprile/PyFocus',  
+  download_url = 'https://github.com/fcaprile/PyFocus/archive/refs/tags/0.5.0.tar.gz',    
   keywords = ['User interface', 'Custom phase mask'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  install_requires=[            
 ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
