fixed minus sign in equations, made GUI adaptable to screen resolution and size

PyFocus/VP_functions.py
@@ -132,11 +132,11 @@ def VP_fields(II1,II2,II3,II4,II5,wavelength,I0,gamma,beta,x_steps,z_steps,x_ran
     #for negative z values there is a minus sign that comes out, and so the first part of the vstack has a - multiplyed
     exx=a1*np.hstack((- np.fliplr(II1)*np.exp(1j*phip) + 0.5*np.fliplr(II2)*np.exp(- 1j*phip) - 0.5*np.fliplr(II3)*np.exp(3j*phip),II1[:,1:rtotalsteps-1]*np.exp(1j*phip) - 0.5*II2[:,1:rtotalsteps-1]*np.exp(- 1j*phip) + 0.5*II3[:,1:rtotalsteps-1]*np.exp(3j*phip)))
     eyx=-0.5*1j*a1*np.hstack((- np.fliplr(II2)*np.exp(- 1j*phip) - np.fliplr(II3)*np.exp(3j*phip),II2[:,1:rtotalsteps-1]*np.exp(- 1j*phip) + II3[:,1:rtotalsteps-1]*np.exp(3j*phip)))
-    ezx=a1*1j*np.hstack((np.fliplr(II4) - np.fliplr(II5)*np.exp(2j*phip),II4[:,1:rtotalsteps-1] - II5[:,1:rtotalsteps-1]*np.exp(2j*phip)))
+    ezx=-a1*1j*np.hstack((np.fliplr(II4) - np.fliplr(II5)*np.exp(2j*phip),II4[:,1:rtotalsteps-1] - II5[:,1:rtotalsteps-1]*np.exp(2j*phip)))
     
     exy=- 0.5*a2*1j*np.hstack((- np.fliplr(II2)*np.exp(- 1j*phip) - np.fliplr(II3)*np.exp(3j*phip),II2[:,1:rtotalsteps-1]*np.exp(- 1j*phip) + II3[:,1:rtotalsteps-1]*np.exp(3j*phip)))
     eyy=a2*np.hstack((- np.fliplr(II1)*np.exp(1j*phip) - 0.5*np.fliplr(II2)*np.exp(- 1j*phip) + 0.5*np.fliplr(II3)*np.exp(3j*phip),II1[:,1:rtotalsteps-1]*np.exp(1j*phip) + 0.5*II2[:,1:rtotalsteps-1]*np.exp(- 1j*phip) - 0.5*II3[:,1:rtotalsteps-1]*np.exp(3j*phip)))
-    ezy=- a2*np.hstack((np.fliplr(II4) + np.fliplr(II5)*np.exp(2j*phip),II4[:,1:rtotalsteps-1] +II5[:,1:rtotalsteps-1]*np.exp(2j*phip)))
+    ezy=a2*np.hstack((np.fliplr(II4) + np.fliplr(II5)*np.exp(2j*phip),II4[:,1:rtotalsteps-1] +II5[:,1:rtotalsteps-1]*np.exp(2j*phip)))
 
     Ex=exx + exy
     Ey=eyx + eyy
@@ -161,10 +161,10 @@ def VP_fields(II1,II2,II3,II4,II5,wavelength,I0,gamma,beta,x_steps,z_steps,x_ran
             rp=int(np.rint(rp))
             exx2[yy,xx]=a1*(II1[zz,rp]*np.exp(1j*phip) - 0.5*II2[zz,rp]*np.exp(-1j*phip) + 0.5*II3[zz,rp]*np.exp(3j*phip))
             eyx2[yy,xx]=- 0.5*a1*1j*(II2[zz,rp]*np.exp(- 1j*phip) + II3[zz,rp]*np.exp(3j*phip))
-            ezx2[yy,xx]=a1*1j*(II4[zz,rp] - II5[zz,rp]*np.exp(2j*phip))
+            ezx2[yy,xx]=-a1*1j*(II4[zz,rp] - II5[zz,rp]*np.exp(2j*phip))
             exy2[yy,xx]=-0.5*a2*1j*(II2[zz,rp]*np.exp(- 1j*phip) +II3[zz,rp]*np.exp(3j*phip))
             eyy2[yy,xx]=a2*(II1[zz,rp]*np.exp(1j*phip) + 0.5*II2[zz,rp]*np.exp(- 1j*phip) - 0.5*II3[zz,rp]*np.exp(3j*phip))
-            ezy2[yy,xx]=- a2*(II4[zz,rp] + II5[zz,rp]*np.exp(2j*phip))
+            ezy2[yy,xx]=a2*(II4[zz,rp] + II5[zz,rp]*np.exp(2j*phip))
     Ex2=exx2 + exy2
     Ey2=eyx2 + eyy2
     Ez2=ezx2 + ezy2

PyFocus/interface.py
@@ -154,7 +154,7 @@ def interface_custom_mask_focus_field_XY(n_list,d_list,ex_lens,ey_lens,alpha,h,w
     prefactor_general=cos_theta_sqrt*sin_theta*k1
     prefactor_x=prefactor_general*(cos_theta+(1-cos_theta)*sin_phi_square)
     prefactor_y=prefactor_general*(1-cos_theta)*cos_phi*sin_phi
-    prefactor_z=prefactor_general*sin_theta*cos_phi
+    prefactor_z=prefactor_general*(-sin_theta*cos_phi)
     
     Axx=-prefactor_x*ex_lens*weight_trapezoid
     Axy=-prefactor_y*ex_lens*weight_trapezoid
@@ -182,7 +182,7 @@ def interface_custom_mask_focus_field_XY(n_list,d_list,ex_lens,ey_lens,alpha,h,w
     #For integration of the reflected and transmited fields (Er and Et):
     prefactor_x_r=-prefactor_general*(-rs_i_theta*sin_phi_square+rp_i_theta*cos_phi_square*cos_theta)
     prefactor_y_r=-prefactor_general*(rs_i_theta+rp_i_theta*cos_theta)*cos_phi*sin_phi
-    prefactor_z_r=prefactor_general*rp_i_theta*sin_theta*cos_phi
+    prefactor_z_r=prefactor_general*rp_i_theta*(-sin_theta*cos_phi)
     
     phase_z_r=np.exp(2*1j*k1*np.cos(theta)*z_int)
     
@@ -207,7 +207,7 @@ def interface_custom_mask_focus_field_XY(n_list,d_list,ex_lens,ey_lens,alpha,h,w
 
     prefactor_x_t=prefactor_general_t*(ts_t_theta*sin_phi_square+tp_t_theta*cos_phi_square*cos_theta_t)
     prefactor_y_t=prefactor_general_t*(-ts_t_theta+tp_t_theta*cos_theta_t)*cos_phi*sin_phi
-    prefactor_z_t=-prefactor_general_t*tp_t_theta*sin_theta_t*cos_phi
+    prefactor_z_t=prefactor_general_t*tp_t_theta*sin_theta_t*cos_phi
     
     phase_z_t=np.exp(1j*z_int*(k2*cos_theta_t+k1*cos_theta))
     
@@ -431,7 +431,7 @@ def interface_custom_mask_focus_field_XZ_XY(n_list,d_list,ex_lens,ey_lens,alpha,
     prefactor_general=cos_theta_sqrt*sin_theta*k1
     prefactor_x=prefactor_general*(sin_phi_square+cos_phi_square*cos_theta)
     prefactor_y=prefactor_general*(-1+cos_theta)*cos_phi*sin_phi
-    prefactor_z=-prefactor_general*sin_theta*cos_phi
+    prefactor_z=prefactor_general*(-sin_theta*cos_phi)
     
     Axx=-prefactor_x*ex_lens*weight_trapezoid
     Axy=prefactor_y*ex_lens*weight_trapezoid
@@ -463,7 +463,7 @@ def interface_custom_mask_focus_field_XZ_XY(n_list,d_list,ex_lens,ey_lens,alpha,
     #For integration of the reflected and transmited fields (Er and Et):
     prefactor_x_r=prefactor_general*(rs_i_theta*sin_phi_square-rp_i_theta*cos_phi_square*cos_theta)
     prefactor_y_r=prefactor_general*(-rs_i_theta-rp_i_theta*cos_theta)*cos_phi*sin_phi
-    prefactor_z_r=-prefactor_general*rp_i_theta*sin_theta*cos_phi
+    prefactor_z_r=prefactor_general*rp_i_theta*(-sin_theta*cos_phi)
     
     phase_z_r=np.exp(2*1j*k1*np.cos(theta)*z_int)
     
@@ -488,7 +488,7 @@ def interface_custom_mask_focus_field_XZ_XY(n_list,d_list,ex_lens,ey_lens,alpha,
 
     prefactor_x_t=prefactor_general_t*(ts_t_theta*sin_phi_square+tp_t_theta*cos_phi_square*cos_theta_t)
     prefactor_y_t=prefactor_general_t*(-ts_t_theta+tp_t_theta*cos_theta_t)*cos_phi*sin_phi
-    prefactor_z_t=-prefactor_general_t*tp_t_theta*sin_theta_t*cos_phi
+    prefactor_z_t=prefactor_general_t*tp_t_theta*sin_theta_t*cos_phi
     
     phase_z_t=np.exp(1j*z_int*(k2*cos_theta_t+k1*cos_theta))
     

PyFocus/no_mask_functions.py
@@ -124,7 +124,7 @@ def no_mask_fields(II1,II2,II3,wavelength,I0,beta,gamma,z_steps,r_steps,x_range,
     #for negative z values there is a minus sign that comes out, and so the first part of the vstack has a - multiplyed
     exx=-a1*1j*np.hstack((np.fliplr(II1)+np.cos(2*phip)*np.fliplr(II3), II1[:,1:rtotalsteps-1]+np.cos(2*phip)*II3[:,1:rtotalsteps-1]))
     eyx=-a1*1j*np.hstack((np.fliplr(II3)*np.sin(2*phip), np.sin(2*phip)*II3[:,1:rtotalsteps-1]))
-    ezx=-a1*2*np.hstack((-np.fliplr(II2)*np.cos(phip), np.cos(phip)*II2[:,1:rtotalsteps-1]))
+    ezx=a1*2*np.hstack((-np.fliplr(II2)*np.cos(phip), np.cos(phip)*II2[:,1:rtotalsteps-1]))
 
     exy=-a2*1j*np.hstack((np.fliplr(II3)*np.sin(2*phip), np.sin(2*phip)*II3[:,1:rtotalsteps-1]))
     eyy=-a2*1j*np.hstack((np.fliplr(II1)-np.cos(2*phip)*np.fliplr(II3), II1[:,1:rtotalsteps-1]-np.cos(2*phip)*II3[:,1:rtotalsteps-1]))
@@ -153,7 +153,7 @@ def no_mask_fields(II1,II2,II3,wavelength,I0,beta,gamma,z_steps,r_steps,x_range,
             rp=int(np.rint(rp))
             exx2[yy,xx]=-a1*1j*(II1[zz,rp]+np.cos(2*phip)*II3[zz,rp])
             eyx2[yy,xx]=-a1*1j*(np.sin(2*phip)*II3[zz,rp])
-            ezx2[yy,xx]=-a1*2*(np.cos(phip)*II2[zz,rp])
+            ezx2[yy,xx]=a1*2*(np.cos(phip)*II2[zz,rp])
             exy2[yy,xx]=-a2*1j*(np.sin(2*phip)*II3[zz,rp])
             eyy2[yy,xx]=-a2*1j*(II1[zz,rp]-np.cos(2*phip)*II3[zz,rp])
             ezy2[yy,xx]=-a2*2*(np.sin(phip)*II2[zz,rp])