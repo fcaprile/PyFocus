Fix integration, fix units

src/custom_dataclasses/field_parameters.py
@@ -1,7 +1,5 @@
-
 from dataclasses import dataclass
 import numpy as np
-
 
 @dataclass
 class PolarizationParameters:
@@ -16,7 +14,9 @@ class FieldParameters:
     polarization: PolarizationParameters
     
     def transform_input_parameter_units(self):
-        #transform to radians:
+        '''transform to radians and from milimeters to nanometers'''
+        self.w0 *= 10**6
+        
         self.polarization.beta *= np.pi/180
         self.polarization.gamma *= np.pi/180
 

src/model/focus_field_calculators/base.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractclassmethod
+from copy import deepcopy
 from pydantic import BaseModel, StrictFloat, StrictInt
 from custom_typing import Matrix
 
@@ -34,14 +35,14 @@ class FocusFieldCalculator(ABC):
         NA: Union[StrictFloat, StrictInt] # Numerical aperture
         n: Union[StrictFloat, StrictInt] # Refraction index for the medium of the optical system.
         h: Union[StrictFloat, StrictInt] # Radius of aperture of the objective lens
-        f: Union[StrictFloat, StrictInt] # Focal distance
-        w0: Union[StrictFloat, StrictInt] # Radius of the incident gaussian beam
+        f: Union[StrictFloat, StrictInt] = 0 # Focal distance
         
-        z: Union[StrictFloat, StrictInt] # Axial position for the XY plane (nm)
         x_steps: Union[StrictFloat, StrictInt] # Resolution in the X or Y coordinate for the focused field (nm)
         z_steps: Union[StrictFloat, StrictInt] # Resolution in the axial coordinate (Z) for the focused field (nm)
         x_range: Union[StrictFloat, StrictInt] # Field of view in the X or Y coordinate in which the focused field is calculated (nm)
         z_range: Union[StrictFloat, StrictInt] # Field of view in the axial coordinate (z) in which the focused field is calculated (nm)
+        
+        z: Union[StrictFloat, StrictInt] # Axial position for the XY plane (nm)
         phip: Union[StrictFloat, StrictInt] # Angle of rotation along the z axis when calculating the field
         
         field_parameters: FieldParameters
@@ -60,13 +61,13 @@ class FocusFieldCalculator(ABC):
 
         
         def transform_input_parameter_units(self):
-            '''transforms input units to nanometers or radians'''
-            self.f*=10**6
-            self.w0*=10**6
+            '''transform to radians and from milimeters to nanometers'''
+            self.field_parameters.transform_input_parameter_units()
+            
+            self.f = self.h * self.n/ self.NA *10**6
             
             #transform to radians:
             self.phip /= 180*np.pi
-            self.field_parameters.transform_input_parameter_units()
 
     @abstractclassmethod
     def calculate(focus_field_parameters: FocusFieldParameters) -> FieldAtFocus:
@@ -83,21 +84,27 @@ class FocusFieldCalculator(ABC):
         return [np.zeros((x_size, y_size),dtype=complex) for _ in range(6)]
 
     def mirror_on_z_axis(self, matrixes):
-        for matrix in matrixes:
-            matrix=np.vstack((np.flipud(np.conj(matrix)),matrix[1:,:]))
-        return matrixes
+        output = []
+        for matrix in deepcopy(matrixes):
+            output.append(np.vstack((np.flipud(np.conj(matrix)),matrix[1:,:])))
+        return output
     
     def integrate(self, matrix_ammount, functions_to_integrate, focus_field_parameters: FocusFieldParameters, description):
         
         matrixes = [np.zeros((focus_field_parameters.ztotalsteps, focus_field_parameters.rtotalsteps),dtype=complex) for _ in range(matrix_ammount)]
         
-        for n_z in tqdm(range(focus_field_parameters.ztotalsteps),desc=description):
+        print(f'{focus_field_parameters.ztotalsteps=}')
+        print(f'{focus_field_parameters=}')
+        print(f'{focus_field_parameters.alpha=}')
+        for n_z in tqdm(range(focus_field_parameters.ztotalsteps),desc=description): #TODO ir ploteando los valores que toma n_z
             for n_r in range(focus_field_parameters.rtotalsteps):
                 kz=n_z*2*np.pi/focus_field_parameters.field_parameters.wavelength/focus_field_parameters.ztotalsteps*focus_field_parameters.z_range/2 
                 kr=n_r*2*np.pi/focus_field_parameters.field_parameters.wavelength/focus_field_parameters.rtotalsteps*focus_field_parameters.x_range/2*2**0.5
                 
-                for i in range(matrix_ammount):
-                    (matrixes[i])[n_z,n_r] = complex_quadrature(functools.partial(functions_to_integrate[i], kz, kr),0,focus_field_parameters.alpha)[0]
+                for i, matrix in enumerate(matrixes):
+                    result = complex_quadrature(functools.partial(functions_to_integrate[i], kz=kz, kr=kr),0,focus_field_parameters.alpha)[0]
+                    #print(f'{i=},{result=}')
+                    (matrixes[i])[n_z,n_r] = result
         
         return matrixes
     

src/plot_functions/plot_at_focus.py
@@ -7,22 +7,22 @@ from plot_functions.plot_polarization_elipses import polarization_elipse
 from model.focus_field_calculators.base import FocusFieldCalculator
 from plot_functions import PlotParameters
 
-def plot_polarization_elipses_on_ax(ax, xmax, values):
+def plot_polarization_elipses_on_ax(ax, xmax, ex_values, ey_values, intensity_values):
     x_pos=np.linspace(-xmax*0.95,xmax*0.95,10)
     y_pos=np.linspace(-xmax*0.95,xmax*0.95,10)
-    x_values=np.linspace(-xmax,xmax,np.shape(values)[0])
-    y_values=np.linspace(xmax,-xmax,np.shape(values)[0])
+    x_values=np.linspace(-xmax,xmax,np.shape(intensity_values)[0])
+    y_values=np.linspace(xmax,-xmax,np.shape(intensity_values)[0])
     AMP=np.abs(xmax/6)
     for x_coor in x_pos:
         for y_coor in y_pos:
             x_index = (np.abs(x_values - x_coor)).argmin()
             y_index = (np.abs(y_values - y_coor)).argmin()
             # if np.abs(Ifield_xy[y_index,x_index]/np.max(Ifield_xy))>0.15 and np.abs(ez_XY[y_index,x_index])**2/np.max(Ifield_xy)<0.9:#added a condition for the z intensity to avoid point where the polarization is mostly along z
-            if np.abs(values[y_index,x_index]/np.max(values))>0.15: #removed the z intensity condition
+            if np.abs(intensity_values[y_index,x_index]/np.max(intensity_values))>0.15: #removed the z intensity condition
                 if x_coor==x_pos[5] and y_coor==y_pos[5]: #at x,y=0,0 usually the atan2 does not work well
                     x_index+=1
                     y_index+=1
-                polarization_elipse(ax,x_coor,y_coor,values[y_index,x_index],values[y_index,x_index],AMP)
+                polarization_elipse(ax,x_coor,y_coor,ex_values[y_index,x_index],ey_values[y_index,x_index],AMP)
 
 def line_plot_on_ax(ax: Axes, title: str, values: list[list], extent: list, horizontal_label: str, vertical_label: str, pad: int = 20):
     ax.set_title(title,pad=pad)
@@ -67,11 +67,11 @@ def plot_intensity_at_focus(focus_field: FocusFieldCalculator.FieldAtFocus, focu
     color_plot_on_ax(fig, fig.add_subplot(spec[0, 0]), 'Intensity on the XZ plane', focus_field.Intensity_XZ, extent_XZ, 'x (nm)', 'z (nm)', 'Intensity (kW/cm\u00b2)', square_axis=False)
     color_plot_on_ax(fig, fig.add_subplot(spec[0, 1]), 'Intensity on the XY plane', focus_field.Intensity_XY, extent_XY, 'x (nm)', 'y (nm)', 'Intensity (kW/cm\u00b2)', square_axis=True)
     
-    line_plot_on_ax(fig.add_subplot(spec[0, 1]), 'Intensity along x', focus_field.Intensity_along_x, extent_X_axis, 'x (nm)', 'Intensity (kW/cm\u00b2)')
+    line_plot_on_ax(fig.add_subplot(spec[0, 2]), 'Intensity along x', focus_field.Intensity_along_x, extent_X_axis, 'x (nm)', 'Intensity (kW/cm\u00b2)')
     
     ax = fig.add_subplot(spec[1, 1])
     color_plot_on_ax(fig, ax, 'Polarization on the XY plane', focus_field.Intensity_XY, extent_XY, 'x (nm)', 'y (nm)', 'Intensity (kW/cm\u00b2)', square_axis=True, alpha=0.5)
-    plot_polarization_elipses_on_ax(ax, xmax=xmax, values=focus_field.Intensity_XY)
+    plot_polarization_elipses_on_ax(ax, xmax=xmax, ex_values=focus_field.Ex_XY, ey_values=focus_field.Ey_XY, intensity_values=focus_field.Intensity_XY)
     
     '''
     #formula to calculate fwhm