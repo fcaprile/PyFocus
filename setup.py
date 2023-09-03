fc906c03a347efb4ac9899dea419007ea3eaf1db
Caprile Fernando <fcaprile@gmail.com>
2023-09-02 23:19:51 -0300

    rename for packaging

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'PyCustomFocus',         
   packages = ['PyFocus'],   
-  version = '3.0',      
+  version = '3.1',      
   license='MIT',        
   description = 'Full vectorial calculation of focused electromagnetic fields moduled by a custom phase mask',   
   author = 'Caprile Fernando',                  
   author_email = 'fcaprile@gmail.com',      
   url = 'https://github.com/fcaprile/PyFocus',  
-  download_url = 'https://github.com/fcaprile/PyFocus/archive/refs/tags/3.0.tar.gz',    
+  download_url = 'https://github.com/fcaprile/PyFocus/archive/refs/tags/3.1.tar.gz',    
   keywords = ['User interface', 'Custom phase mask'],   # Keywords that define your package best
   install_requires=[            
 ],
