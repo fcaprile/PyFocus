fix unit conversion in intensity factor

src/PyFocus/model/focus_field_calculators/custom_mask.py
@@ -601,11 +601,14 @@ class CustomMaskFocusFieldCalculator(FocusFieldCalculator):
 
     def calculate_incident_energy_ratio(self, lens_aperture, mask_function,w0,f,k):
         """Ratio between the energy inciding on the pupil from an uniform field of amplitude 1 (E_unif) 
-        and the inciding energy from the custom incident field (E_inc).
+        and the inciding energy from the custom incident field (E_inc). 
         
-        The nergy inciding on the pupyl is calculated as the integral over the pupil's surface 
+        IMPORTANT: mask_function must be in nm, so keep in mind passage from mm to nm
+        
+        The energy inciding on the pupyl is calculated as the integral over the pupil's surface 
         (in our case, a circle) of the inciding field.
         """
+        lens_aperture*=10**6 # Unit passage from mm to n
         E_unif = np.pi*(lens_aperture**2)
 
         logger.debug(f"Calculating incident energy ratio for {lens_aperture=}...")
@@ -613,11 +616,11 @@ class CustomMaskFocusFieldCalculator(FocusFieldCalculator):
         divisions = 300 # The higher the number of divisions the precision improvs but calculation time gets longer
         weight_trapezoid = self._calculate_2D_trapezoidal_method_weight(lens_aperture, divisions, divisions)
         
-        rho_values=np.linspace(0,lens_aperture,divisions, dtype=complex) #divisions of rho in which the trapezoidal 2D integration is done
-        phi_values=np.linspace(0,2*np.pi,divisions, dtype=complex) #divisions of phi in which the trapezoidal 2D integration is done
+        rho_values=np.linspace(0,lens_aperture,divisions) #divisions of rho in which the trapezoidal 2D integration is done
+        phi_values=np.linspace(0,2*np.pi,divisions) #divisions of phi in which the trapezoidal 2D integration is done
         
         # Function to integrate, the energy is the integral of the incident intensity
-        I_inc = np.zeros((divisions, divisions), dtype=complex)
+        I_inc = np.zeros((divisions, divisions))
         for i,phi in enumerate(phi_values):
             for j,rho in enumerate(rho_values):
                 I_inc[i,j]=np.abs(mask_function(rho,phi,w0,f,k))**2

tests/unit/test_intensity_normalization.py
@@ -13,7 +13,6 @@ from src.PyFocus.custom_dataclasses.mask import MaskType
 
 def test_normalization_of_uniform_beam():
     logger.setLevel(logging.INFO)
-    do_a_precise_simulation_and_plot_it = False
     
     calculation_handler = MainCalculationHandler(mask_type=MaskType.custom_mask)
     lens_apperture = 3
@@ -23,10 +22,20 @@ def test_normalization_of_uniform_beam():
 
 def test_normalization_of_custom_beam():
     logger.setLevel(logging.INFO)
-    do_a_precise_simulation_and_plot_it = False
     
     calculation_handler = MainCalculationHandler(mask_type=MaskType.custom_mask)
     lens_apperture = 3
-    mask_function = lambda rho, phi, w0,f,k : (rho)**0.5*np.exp(1j*phi)
+    mask_function = lambda rho, phi, w0,f,k : (rho/(3*10**6))**0.5*np.exp(1j*phi)
     ratio = calculation_handler.calculate_incident_energy_ratio(lens_apperture, mask_function, None, None, None)
     print(ratio)
+
+def test_normalization_of_gaussian_beam():
+    logger.setLevel(logging.INFO)
+    
+    calculation_handler = MainCalculationHandler(mask_type=MaskType.custom_mask)
+    lens_apperture = 3
+    waist_values = [0.05,0.1,0.5,1,2,3,5,10]
+    for waist in waist_values:
+        mask_function = lambda rho, phi, w0,f,k : (lens_apperture/waist)*np.exp(-(rho/(waist*10**6))**2/2)
+        ratio = calculation_handler.calculate_incident_energy_ratio(lens_apperture, mask_function, None, None, None)
+        print(f"{waist=}, {ratio=}")